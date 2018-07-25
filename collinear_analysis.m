0cbd4f9c6256e651042219f75f90cf0809301df4
Hollanda Arief Kusuma <hollandacocobear@gmail.com>
2018-07-25 09:27:08 +0700

    Update collinear_analysis.m

--- collinear_analysis.m
+++ collinear_analysis.m
@@ -1,5 +1,7 @@
 clear all;clc;
 
-flist={'GFO-1_a_dsc'};
-fout='GFO-1_a_col_dsc';
+flist={'TOPEX_a_dsc','JASON-1_a_dsc','JASON-2_a_dsc','JASON-3_a_dsc'};
+%flist={'GFO-1_a_col_dsc'};
+%fout='GFO-1_a_col_dsc';
+fout='TPJ_a_col_dsc';
 collinearf(flist,fout);
